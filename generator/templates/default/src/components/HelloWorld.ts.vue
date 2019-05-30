vuetifyjs/vue-cli-plugins

commit d93e60b820711fda6079426ca89c137b5c004723

feat: generate typescript vue files for ts projects
fixes #66

generator/templates/default/src/components/HelloWorld.ts.vue
@@ -1,0 +1,145 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </p>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in assets.whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(link, i) in assets.importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(eco, i) in assets.ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader'
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify'
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+      }
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com'
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com'
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuetifyjs.com'
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs'
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify'
+      }
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer'
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined'
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+      }
+
+    ]
+  })
+})
+</script>

generator/templates/default/src/App.ts.vue
@@ -1,0 +1,56 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <%_ if (options.iconFont === 'mdi') { _%>
+        <v-icon>mdi-open-in-new</v-icon>
+        <%_ } else if (options.iconFont === 'md') { _%>
+        <v-icon>open_in_new</v-icon>
+        <%_ } else if (options.iconFont === 'fa') { _%>
+        <v-icon>fas fa-external-link-alt</v-icon>
+        <%_ } else if (options.iconFont === 'fa4') { _%>
+        <v-icon>fa-external-link</v-icon>
+        <%_ } _%>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <%_ if (options.router) { _%>
+      <router-view/>
+      <%_ } else { _%>
+      <HelloWorld/>
+      <%_ } _%>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+<%_ if (!options.router) { _%>
+import HelloWorld from './components/HelloWorld.vue'
+<%_ } _%>
+
+export default Vue.extend({
+  name: 'App',
+  <%_ if (!options.router) { _%>
+  components: {
+    HelloWorld
+  },
+  <%_ } _%>
+  data () {
+    return {
+      //
+    }
+  }
+})
+</script>

generator/templates/default/src/components/HelloWorld.js.vue
@@ -85,63 +85,59 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
+export default {
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader'
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify'
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+      }
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com'
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com'
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuetifyjs.com'
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs'
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify'
+      }
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer'
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined'
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+      }
 
-      ]
-    })
-  }
+    ]
+  })
+}
 </script>
-
-<style>
-
-</style>

generator/templates/default/src/views/Home.js.vue
@@ -1,0 +1,13 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>

generator/templates/default/src/views/Home.ts.vue
@@ -1,0 +1,14 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default Vue.extend({
+  components: {
+    HelloWorld
+  }
+})
+</script>

generator/templates/default/src/views/Home.vue
@@ -1,13 +1,0 @@
-<template>
-  <HelloWorld />
-</template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>

generator/tools/vuetify.js
@@ -9,10 +9,11 @@ function addDependencies (api) {
 }
 
 function renderFiles (api, opts) {
-  const pluginFilename = api.hasPlugin('typescript') ? 'vuetify.ts' : 'vuetify.js'
-  const pluginSourceFilename = 'vuetify.js'
+  const ext = api.hasPlugin('typescript') ? 'ts' : 'js'
+  const pluginFilename = `vuetify.${ext}`
+
   api.render({
-    [`./src/plugins/${pluginFilename}`]: `../templates/default/src/plugins/${pluginSourceFilename}`
+    [`./src/plugins/${pluginFilename}`]: '../templates/default/src/plugins/vuetify.js'
   }, {
     ...opts,
     typescript: api.hasPlugin('typescript')
@@ -20,18 +21,18 @@ function renderFiles (api, opts) {
 
   // Render files if we're replacing
   const fs = require('fs')
-  const routerPath = api.resolve('./src/router.js')
+  const routerPath = api.resolve(`./src/router.${ext}`)
   opts.router = fs.existsSync(routerPath)
 
   if (opts.replaceComponents) {
     const files = {
-      './src/App.vue': '../templates/default/src/App.vue',
+      './src/App.vue': `../templates/default/src/App.${ext}.vue`,
       './src/assets/logo.svg': '../templates/default/src/assets/logo.svg',
-      './src/components/HelloWorld.vue': '../templates/default/src/components/HelloWorld.vue'
+      './src/components/HelloWorld.vue': `../templates/default/src/components/HelloWorld.${ext}.vue`
     }
 
     if (opts.router) {
-      files['./src/views/Home.vue'] = '../templates/default/src/views/Home.vue'
+      files['./src/views/Home.vue'] = `../templates/default/src/views/Home.${ext}.vue`
     }
 
     api.render(files, opts)

danielthomas3963/laravel-vue-cli-plugins-develop

commit 0f2c59a7a98d2faf0c921c8e6b5c2b33c6880f66

fix: revert bug introduced in d93e60b820711fda6079426ca89c137b5c004723

generator/templates/default/src/components/HelloWorld.ts.vue
@@ -32,7 +32,7 @@
 
         <v-layout justify-center>
           <a
-            v-for="(next, i) in assets.whatsNext"
+            v-for="(next, i) in whatsNext"
             :key="i"
             :href="next.href"
             class="subheading mx-3"
@@ -51,7 +51,7 @@
 
         <v-layout justify-center>
           <a
-            v-for="(link, i) in assets.importantLinks"
+            v-for="(link, i) in importantLinks"
             :key="i"
             :href="link.href"
             class="subheading mx-3"
@@ -70,7 +70,7 @@
 
         <v-layout justify-center>
           <a
-            v-for="(eco, i) in assets.ecosystem"
+            v-for="(eco, i) in ecosystem"
             :key="i"
             :href="eco.href"
             class="subheading mx-3"